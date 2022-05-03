6e6e95a18f8c1a6193b20fc2d2893a0f3621551a
Zahra Arshia <fzarshia@gmail.com>
2022-05-03 15:39:19 +0430

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS public.animals(
 	name VARCHAR,
 	date_of_birth DATE,
 	escape_attempts INT DEFAULT 0,
-	neutered BOOLEAN NOT NULL,
+	neutered BOOLEAN,
     weight_kg DECIMAL
 );
 
