b37123b8375570ea1ab78777e06f84b0c4a952b1
Zahra Arshia <fzarshia@gmail.com>
2022-05-03 19:00:00 +0430

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -5,12 +5,10 @@ CREATE DATABASE vet_clinic
     CONNECTION LIMIT = -1;
 
 CREATE TABLE IF NOT EXISTS public.animals(
-	Id SERIAL PRIMARY KEY,
-	name VARCHAR,
+	id INT GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(100),
 	date_of_birth DATE,
 	escape_attempts INT DEFAULT 0,
 	neutered BOOLEAN,
-    weight_kg DECIMAL
+    	weight_kg DECIMAL
 );
-
-SELECT * FROM animals;
