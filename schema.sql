42a2fbf7f918b4403a79edf420327f2f7ee4d259
Zahra Arshia <fzarshia@gmail.com>
2022-05-04 14:32:00 +0430

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -15,3 +15,24 @@ CREATE TABLE IF NOT EXISTS public.animals(
 
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(200);
+
+CREATE TABLE public.owners
+(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name character varying(200),
+    age integer,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE public.species
+(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name character varying(200),
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE animals DROP COLUMN IF EXISTS species;
+ALTER TABLE animals ADD COLUMN species_id INT;
+ALTER TABLE animals ADD CONSTRAINT species_fkey FOREIGN KEY (species_id) REFERENCES species(id) ON DELETE CASCADE;
+ALTER TABLE animals ADD COLUMN owners_id INT;
+ALTER TABLE animals ADD CONSTRAINT owners_fkey FOREIGN KEY (owners_id) REFERENCES owners(id) ON DELETE CASCADE;
